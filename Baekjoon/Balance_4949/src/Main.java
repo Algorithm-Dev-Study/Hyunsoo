43b19ef6ae5f1a89934e660fdb5b6f429862e340
myhuon <myhuon@naver.com>
2023-01-23 20:25:18 +0900

    균형 잡힌 세상 No.4949 Success

---
+++ Baekjoon/Balance_4949/src/Main.java
@@ -0,0 +1,42 @@
+/*
+ Link : https://www.acmicpc.net/problem/4949
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+	
+	public static String isBalance(String str) {
+		ArrayList<Character> opp = new ArrayList<Character>();
+		int oppSize = 0;
+		for(int i = 0; i < str.length(); i++) {
+			oppSize = opp.size();
+			char c = str.charAt(i);
+			if(c == '(' || c == '[') {
+				opp.add(c);
+			} else if(c == ')') {
+				if(oppSize == 0 || opp.remove(oppSize - 1) != '(') {
+					return "no";
+				}
+			} else if(c == ']') {
+				if(oppSize == 0 || opp.remove(oppSize - 1) != '[') {
+					return "no";
+				}
+			}
+		}
+		if(oppSize == 0) return "yes";
+		else return "no";
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String text = "";
+		while(true) {
+			text = br.readLine();
+			if(text.equals(".")) break;
+			System.out.println(isBalance(text));
+		}
+	}
+}
