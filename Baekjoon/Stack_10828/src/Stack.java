9efe34b8245f7150b928e62f3029e8bf604ad66a
myhuon <myhuon@naver.com>
2023-01-23 14:28:12 +0900

    stack and tree

---
+++ Baekjoon/Stack_10828/src/Stack.java
@@ -0,0 +1,76 @@
+/*
+ * Link : https://www.acmicpc.net/problem/10828
+ * 
+ * 백준 시간초과 해결 -> Stack_v2
+ * 
+*/
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Stack {
+	static ArrayList<Integer> listStack = new ArrayList<>();
+	static int stackSize = 0;
+	
+	private static int pop () {
+		if(stackSize == 0) return -1;
+		return listStack.remove(stackSize - 1);
+	}
+	
+	private static void push(int num) {
+		listStack.add(num);
+	}
+	
+	private static int isEmpty() {
+		if(stackSize == 0) return 1;
+		return 0;
+	}
+	
+	private static int getTop() {
+		if(stackSize == 0) return -1;
+		return listStack.get(stackSize - 1);
+	}
+	
+	private static int doAction(String[] arrInput) {
+		int result = -2;
+		
+		switch(arrInput[0]) {
+		case "pop" :
+			result = pop();
+			if(result != -1) stackSize--;
+			break;
+		case "push" :
+			push(Integer.valueOf(arrInput[1]));
+			stackSize++;
+			break;
+		case "size" :
+			result = stackSize;
+			break;
+		case "empty" :
+			result = isEmpty();
+			break;
+		case "top" :
+			result = getTop();
+			break;			
+		}
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.nextLine();
+		
+		String strInput = "";
+		int result = 0;
+		String[] arrInput = new String[2];
+		for(int i = 0; i < num; i++) {
+			strInput = sc.nextLine();
+			arrInput = strInput.split(" ");
+			result = doAction(arrInput);
+			if (result != -2) System.out.println(result);
+		}
+		
+		sc.close();
+	}
+}
---
+++ Baekjoon/Stack_10828/src/Stack_v2.java
@@ -0,0 +1,64 @@
+/*
+ * Link : https://www.acmicpc.net/problem/10828
+ * 
+ * 백준 시간초과 해결
+ * 1. 메소드 통합
+ * 2. Scanner -> BufferedReader 변경 
+ * 
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Stack_v2 {
+	static ArrayList<Integer> listStack = new ArrayList<>();
+	static int stackSize = 0;
+	
+	private static int doAction(String[] arrInput) {
+		int result = -2;
+		
+		switch(arrInput[0]) {
+		case "pop" :
+			if(stackSize == 0) {
+				result = -1;
+			} else {
+				result = listStack.remove(stackSize - 1);
+				stackSize--;
+			}
+			break;
+		case "push" :
+			listStack.add(Integer.valueOf(arrInput[1]));
+			stackSize++;
+			break;
+		case "size" :
+			result = stackSize;
+			break;
+		case "empty" :
+			if(stackSize == 0) result = 1;
+			else result = 0;
+			break;
+		case "top" :
+			if(stackSize == 0) result = -1;
+			else result = listStack.get(stackSize - 1);
+			break;			
+		}
+		
+		return result;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.valueOf(br.readLine());
+		
+		String strInput = "";
+		int result = 0;
+		String[] arrInput = new String[2];
+		for(int i = 0; i < num; i++) {
+			strInput = br.readLine();
+			arrInput = strInput.split(" ");
+			result = doAction(arrInput);
+			if (result != -2) System.out.println(result);
+		}
+	}
+}
